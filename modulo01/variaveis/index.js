6c24f2a19647dc337c6ea3bb3718e13936c6397c
matheuscastro77 <matheuscastro1997@hotmail.com>
2021-11-29 18:16:44 -0300

    variaveis-completo

---
+++ modulo01/variaveis/index.js
@@ -0,0 +1,61 @@
+
+/* Reposta da 1
+console.log(b) a reposta é 10
+console.log (a,b) a resposta é 10,5 */
+
+/* Resposta da 2
+console.log(a,b,c) a resposta é 10,10,10 */
+
+
+/* let horaPorDia = prompt("Quantas horas você trabalha por dia?")
+let valorPorDia = prompt("Quanto você recebe por dia?")
+const horaPorDiaNumero = Number(horaPorDia)
+const valorPorDiaNumero = Number(valorPorDia)
+alert(`Voce recebe ${horaPorDia/valorPorDia} por hora`) */
+
+// programa código
+
+/* let nome
+let idade
+console.log("nome", typeof nome) 
+console.log("idade", typeof idade)  */
+
+/* Foi impresso como undefined porque não ficou definido valores
+para nenhuma das variaveis */
+
+/* nome = prompt("Qual o seu nome?")
+idade = prompt("Qual a sua idade?")
+console.log(nome)
+console.log(idade) */
+
+/* Agora o console mostra os valores que são atribuídos ao site quando são solitados
+já que defeni que o o valor de idade e nome respectivamente vão ser de acordo 
+com o que vai ser preenchido com prompt */
+
+/* console.log("Olá", nome, "você tem", idade, "anos") */
+
+
+// pergunta
+
+
+/* let pergunta1 = "Você está com frio?"
+let pergunta2 = "Você vai embora hoje?"
+let pergunta3 = "Você gosta de pizza?"
+
+let resposta1 = prompt(pergunta1)
+let resposta2 = prompt(pergunta2)
+let resposta3 = prompt(pergunta3)
+console.log(pergunta1,resposta1)
+console.log(pergunta2,resposta2)
+console.log(pergunta3,resposta3)
+ */
+
+/* let a = 10
+let b = 25
+let c 
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b)  */
