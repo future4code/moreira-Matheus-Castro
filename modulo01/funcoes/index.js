e141cda6d18fee3681f78d6f2203617b561b86ad
matheuscastro77 <matheuscastro1997@hotmail.com>
2021-12-02 16:48:54 -0300

    funcoes-incompleto

---
+++ modulo01/funcoes/index.js
@@ -0,0 +1,62 @@
+// 1
+
+/* function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2)) // 10
+console.log(minhaFuncao(10)) // 50 
+// a) será exibido o valor 10 e 50 */
+// b) o função é executada, porém, não será exibida(impressa) no console
+
+// 2
+// a)
+/* let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) */
+
+// A funcão serve para colocar toda a frase em letra minuscula (toLowerCase)
+// e também para confirmar a palavra cenoura está inclusa na frase.
+
+// b)
+/* i.   Eu gosto de cenoura // true
+ii.  CENOURA é bom pra vista // true
+iii. Cenouras crescem na terra */ // true
+
+//////////////////////////////////////////////////////////////////
+
+/*  function sobreMim() {
+	
+	console.log("Eu sou Matheus tenho 24 anos, moro em Ervália e sou estudante")
+}
+
+console.log(sobreMim)
+ */
+
+// 2
+
+/* function sobrePessoa(nome, idade, endereço, profissao) {
+	console.log("Eu sou", nome, ",", "tenho", idade, "anos, moro em", endereço, "e sou", profissao)
+	
+}
+const informacoes = sobrePessoa(prompt("Qual o seu nome?"), prompt("Qual a sua idade?"), prompt("Qual a cidade em que você mora?"), prompt("Qual a sua profissão?", "."))
+
+
+ */
+
+// 3
+
+function contas(resultado){
+	const resultado = numero1 + numero2
+	const resultado = numero1 - numero2
+	console.log(resultados)
+	return
+
+
+}
+
