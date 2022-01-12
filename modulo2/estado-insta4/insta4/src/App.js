f78f3e1cdb3ae6caebfc8f4b31fa31f81530c777
matheuscastro77 <matheuscastro1997@hotmail.com>
2022-01-12 17:08:36 -0300

    projeto insta4

---
+++ modulo2/estado-insta4/insta4/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://st.depositphotos.com/1771835/2035/i/600/depositphotos_20355973-stock-photo-portrait-real-high-definition-grey.jpg'}
+          fotoPost={'https://i.picsum.photos/id/516/200/150.jpg?hmac=J8NuASCG3b9RNBpYgZkAzTPlhuTy5utZiGDzlB2mI2g'}
+        />
+
+        <Post
+          nomeUsuario={'Matheus'}
+          fotoUsuario={'https://st.depositphotos.com/1224365/2408/i/600/depositphotos_24084437-stock-photo-portrait-of-a-normal-boy.jpg'}
+          fotoPost={'https://i.picsum.photos/id/282/200/151.jpg?hmac=785voQ6RAa1s57G1YDCJpubyZG98GhceTb4fkprByT0'}
+        />
+
+        <Post
+          nomeUsuario={'Daniel'}
+          fotoUsuario={'https://static6.depositphotos.com/1003580/615/i/600/depositphotos_6151014-stock-photo-african-american-man-portrait.jpg'}
+          fotoPost={'https://i.picsum.photos/id/727/200/152.jpg?hmac=DjnIkqpqWLA-r5tZP7fJkUt7Tp2Yf7CYEvEILSG6WWE'}
+        />
+      </MainContainer>
+
+
+    );
+  }
+}
+
+export default App;
