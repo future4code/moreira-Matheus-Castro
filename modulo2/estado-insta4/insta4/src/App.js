d63b9e681f5e5c5909c84dae58c28a7db0ae4434
matheuscastro77 <matheuscastro1997@hotmail.com>
2022-01-13 18:54:15 -0300

    erros corrigidos e projeto do insta4 terminado

--- modulo2/estado-insta4/insta4/src/App.js
+++ modulo2/estado-insta4/insta4/src/App.js
@@ -9,6 +9,22 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const EstiloInput = styled.input `
+  padding: 5px;
+  margin: 5px;
+  align-items: center;
+  border-radius: 10px;
+  margin-bottom: 40px;
+`
+const EstiloButton = styled.button `
+  padding: 5px;
+  border-radius: 10px;
+  margin: 10px;
+`
+
+const EstiloH1 = styled.h1 `
+  text-align: center;
+`
 class App extends React.Component {
 
   state = {
@@ -29,24 +45,82 @@ class App extends React.Component {
         fotoUsuario: 'https://static6.depositphotos.com/1003580/615/i/600/depositphotos_6151014-stock-photo-african-american-man-portrait.jpg',
         fotoPost: 'https://i.picsum.photos/id/727/200/152.jpg?hmac=DjnIkqpqWLA-r5tZP7fJkUt7Tp2Yf7CYEvEILSG6WWE'
       }
-    ]
+    ],
+    valorInputUsuario: "",
+    valorInputPerfil: "",
+    valorInputPost: ""
+  }
+
+  adicionaUsuario = () => {
+    
+    const novoUsuario = {
+      
+      nomeUsuario: this.state.valorInputUsuario,
+      
+      fotoUsuario: this.state.valorInputPerfil,
+
+      fotoPost: this.state.valorInputPost
+    };
+    const novosUsuarios = [...this.state.publi, novoUsuario];
+
+    this.setState({publi: novosUsuarios})
+  }
+
+  onChangeInputUsuario = (event) =>{
+    
+    this.setState({valorInputUsuario: event.target.value})
+  }
+
+  onChangeInputFoto = (event) =>{
+    
+    this.setState({valorInputPerfil: event.target.value})
+  }
+
+  onChangeInputPost = (event) =>{
+    
+    this.setState({valorInputPost: event.target.value})
   }
 
   render() {
     const listaDePublis = this.state.publi.map((usuario, index) => {
 
       return (
-        <Post>
+        <Post
           nomeUsuario={usuario.nomeUsuario}
           fotoUsuario={usuario.fotoUsuario}
           fotoPost={usuario.fotoPost}
-
-        </Post>
+        />
       )
     })
 
     return (
       <MainContainer>
+        <span>
+          <EstiloH1>Adicione o seu Post</EstiloH1>
+          <EstiloInput
+            
+            value={this.state.valorInputUsuario}
+            
+            onChange={this.onChangeInputUsuario}
+            placeholder={"Nome"}
+          />
+          <EstiloInput
+            
+            value={this.state.valorInputPerfil}
+            
+            onChange={this.onChangeInputFoto}
+            placeholder={"Link da Foto"}
+          />
+
+          <EstiloInput
+            
+            value={this.state.valorInputPost}
+            
+            onChange={this.onChangeInputPost}
+            placeholder={"Link do Post"}
+          />
+          <EstiloButton onClick={this.adicionaUsuario}>Adicionar</EstiloButton>
+          </span>
         {listaDePublis}
       </MainContainer>
 
